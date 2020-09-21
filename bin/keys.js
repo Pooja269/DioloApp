23b22d6641599cdbeb31555b1ebcaa23e38b8132
pooja <Pooja269@users.noreply.github.com>
2020-09-21 17:00:58 +0530

    Update keys.js

--- bin/keys.js
+++ bin/keys.js
@@ -2,5 +2,5 @@ module.exports={
     mongoUrl:'mongodb://localhost:27017/Diolo',
     RESET_PASSWORD_KEY:'resetepassword42695',
     CLIENT_URL:'http://localhost:3000',
-    SENDGRID_API_KEY:'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'
+    SENDGRID_API_KEY:'API key'
 }
